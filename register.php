fb26fb3f5a25f37c31a42c04586a8fa6a98c3ebd
shirantha23 <shiranthaf103@gmail.com>
2025-01-23 10:16:40 +0530

    2nd commit

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ $db = 'hotel_management';
 $user = 'root'; // Replace with your database username
 $pass = ''; // Replace with your database password
 
+
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
